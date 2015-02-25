bc04d7c3a0b5a3a6ee42bb2f206d19c4ce6ba748
harrysethi <sethi.harinder@gmail.com>
2015-02-25 22:26:22 +0530

    subgraph part left

---
+++ src/drug/globals/Globals.java
@@ -0,0 +1,15 @@
+package drug.globals;
+
+import java.util.Map;
+
+public class Globals {
+
+	public static int graph[][];
+	public static int numOfV;
+	public static int numOfE;
+	public static int in_K;
+
+	public static Map<String, Integer> l_map;
+	
+	public static int numOfConstraints;
+}
